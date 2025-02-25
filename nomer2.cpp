00d86f38a016e22b462d8ea5d7e179f903e2600a
Veniamin2332 <Veniamin1404@mail.ru>
2025-02-26 02:10:33 +0400

    Секретный рецепт Крабсбургера

--- nomer2.cpp
+++ nomer2.cpp
@@ -6,32 +6,26 @@
 #define MAX 1000000
 
 
-int find_O_n(int gr, int (&arr)[N], int x) {
-    int rez = 0;
-    for(; rez < gr; ++rez) 
-        if(arr[rez] == x) return rez;
-    return -1;
-}
+void ftn(int arr[], int gr, int a) {
+    int left = 0;     
+    int right = gr - 1;
 
-int find_O_logn(int gr, int (&arr)[N], int x) {
-    bool flag = false;
-    int l = 0;  
-    int r = gr-1; 
-    int mid;
-    while ((l <= r) && (flag != true) && r <= gr && l >= 0) {
-        mid = (l + r) / 2;  
-        if (arr[mid] == x) flag = true;  
-        else if (arr[mid] > x) r = mid - 1; 
-        else l = mid + 1;
+    while (left < right) {
+        int sum = arr[left] + arr[right];
+        if (sum == a) {
+            //std::cout << left << " и " << right << std::endl;
+            return;
+        }
+        if (sum < a) left++;
+        else right--;
     }
-    if(flag) return mid;
-    return -1;
 }
 
 int main(){
-    int x[N];
+    
     std::default_random_engine rng(time(NULL));
     int max_rand[5] = {10000, 100000, 500000, 1000000, 1500000};
+    int x[N];
 
     for(int j = 0; j < 5; ++j){
         std::cout << std::endl <<  "max: " << max_rand[j] << std::endl;
@@ -40,10 +34,9 @@ int main(){
             for(unsigned i = 0; i < cnt; ++i) x[i] = dstr(rng);
 
             auto begin = std::chrono::steady_clock::now();
-            for(unsigned j = 100000; j != 0; --j)
+            for(unsigned j = 10000; j != 0; --j)
 
-            //find_O_logn(cnt, x, dstr(rng)); //                        тут исследуемая функция
-            find_O_n(cnt, x, dstr(rng));
+            ftn(x, cnt, dstr(rng)); //                        тут исследуемая функция
 
             auto end = std::chrono::steady_clock::now();
             auto time_span = std::chrono::duration_cast <std::chrono::milliseconds> (end - begin);
